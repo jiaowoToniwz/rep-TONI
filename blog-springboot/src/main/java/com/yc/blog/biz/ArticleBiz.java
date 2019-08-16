7be3b9b5ffb322739edcad1ebb2ec570c3dd6be8
jiaowoToniwz <54107365+jiaowoToniwz@users.noreply.github.com>
2019-08-16 21:50:56 +0800

    0816 更新

---
+++ blog-springboot/src/main/java/com/yc/blog/biz/ArticleBiz.java
@@ -0,0 +1,41 @@
+package com.yc.blog.biz;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.pagehelper.PageHelper;
+import com.yc.blog.bean.Article;
+import com.yc.blog.bean.ArticleExample;
+import com.yc.blog.dao.ArticleMapper;
+@Service
+public class ArticleBiz{
+	@Autowired
+	private ArticleMapper am;
+	public List<Article>queryNewArticle(int page){
+		ArticleExample example = new ArticleExample();
+		example.setOrderByClause("createTime desc");
+		PageHelper.startPage(page, 5);
+		return am.selectByExampleWithBLOBs(example);
+	}
+	
+	public List<Article> queryByCategory(int id,int page){
+		ArticleExample example = new ArticleExample();
+		example.createCriteria().andCategoryidEqualTo(id);
+		example.setOrderByClause("createTime desc");
+		PageHelper.startPage(page, 5);
+		return am.selectByExampleWithBLOBs(example);
+	}
+	
+	@Transactional
+	public Article read(int id) {
+		ArticleExample example = new ArticleExample();
+		example.createCriteria().andIdEqualTo(id);
+		Article a = am.selectByPrimaryKey(id);
+		a.setReadcnt((a.getReadcnt()==null ? 0 : a.getReadcnt())+1);
+		am.updateByPrimaryKey(a);
+		return a;
+	}
+}
---
+++ blog-springboot/src/main/java/com/yc/blog/biz/CategoryBiz.java
@@ -0,0 +1,20 @@
+package com.yc.blog.biz;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.yc.blog.bean.Category;
+import com.yc.blog.dao.CategoryMapper;
+@Service
+public class CategoryBiz {
+	@Resource
+	private CategoryMapper cm;
+	
+	public List<Category> queryAll(){
+		return cm.selectByExample(null);
+	}
+
+}
